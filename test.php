8880fe272d72fab13fe2964641c3f7e036167323
Cour ZHAO <cour@live.cn>
2019-09-06 17:43:42 +0800

    Modify test.php

--- test.php
+++ test.php
@@ -33,20 +33,44 @@ $dataaddr = array(
 );
 $datakeys = array('dryer' => array_keys($dataaddr['dryer']));
 
+/**
+ * pack/unpack的模板字符含義
+ * a 一個填充空的字符串
+ * A 一個填充空格的字符串
+ * b 一個位串，在每個字節里位的順序都是升序
+ * B 一個位串，再每個字節裡位的順序都是降序
+ * c 一個有符號char（8位整數）值
+ * C 一個無符號char（8位整數）值    ascii轉字符串
+ * h 一個十六進制串，低四位在前
+ * H 一個十六進制串，高四位在前
+ * s 一个有符号短整数值，16位
+ * S 一个無符号短整数值，16位
+ * 
+ * 1.每个字母后面都可以跟着一个数字，表示 count（计数），如果 count 是一个 * 表示剩下的所有东西。
+ * 2.如果你提供的参数比 $format 要求的少，pack 假设缺的都是空值。如果你提供的参数比 $format 要求的多，那么多余的参数被忽略
+ */
 Events::saveCycle('clintid001');
+//心跳包$$$$，及設備ID  format=a*
+$message = "$$$0001";
+//$message = unpack("C*",$message);
+//$message = pack("C*",0x64,0x37,0x38,0x04,0x04,0x04,0x04,0x04);
 
-//$message = "$$$0001";
-$message = pack("C*",0x64,0x37,0x38,0x04,0x04,0x04,0x04,0x04);
+//設備數據地址  format=H2h1/H2h2/H2h3
 //$message = '010300640001C5D5';
-$message = '$GPRMC,225530.000,A,3637.26040,N,11700.56340,E,10.000,97.17,220512,,,D*57';
+//$message = pack("H*",$message);
+
+//GPS   format=a*
+//$message = '$GPRMC,225530.000,A,3637.26040,N,11700.56340,E,10.000,97.17,220512,,,D*57';
 var_dump($message);
 //var_dump(pack("H*",$message)); 
-$amsg = unpack("C*", $message);
+
+
+$amsg = unpack("a*", $message);
 
 $data = join($amsg);
 $head = substr($data, 0, 3);
 $eid = substr($data, 3, 4);
-var_dump(array($amsg[0],$data, $head, $eid));
+var_dump(array($amsg,$data, $head, $eid));
 
 //Dryer 心跳包
 if ($head == '$$$') {
